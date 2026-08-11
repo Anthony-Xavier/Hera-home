37d0459a315353e8efe3f86c6ea74dc1a1430a62
Anthony xavier <anthonyltx12@gmail.com>
2026-08-11 09:59:00 -0300

    Texto

--- src/Components/Principal/Principal.jsx
+++ src/Components/Principal/Principal.jsx
@@ -10,15 +10,10 @@ const Principal = () => {
 
         <div className="principal__conteudo">
           <h2 className="principal__titulo">
-            Projetando lares com <span>alma</span> e propósito. Móveis que
-            atravessam gerações.
+            Madeira. <span>Design</span>. História.
+            <br />
+            Móveis feitos para atravessar gerações.
           </h2>
-
-          <p className="principal__texto">
-            Design atemporal em madeira de alta qualidade, feito sob medida para
-            o seu espaço. Marcenaria artesanal inspirada na elegância clássica e
-            executada com a precisão do mobiliário moderno.
-          </p>
         </div>
       </div>
       <div className="principal__botoes">
